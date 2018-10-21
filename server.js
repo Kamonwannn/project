2eee15fe9013eff1c6bbd5f89cf497178f9f7589
kamonwan <kamonwan.5034@gmail.com>
2018-10-21 23:08:34 +0700

    ed

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ db.close();
 
      
 //insert data product
-app.post('/product/insert', function(req, res) {
+app.post('/product/inst_product', function(req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;

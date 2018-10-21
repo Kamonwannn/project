511729f52d270af821cf84ab26842d57e1f35955
kamonwan <kamonwan.5034@gmail.com>
2018-10-22 02:22:34 +0700

    er

--- server.js
+++ server.js
@@ -129,6 +129,27 @@ app.post('/products/delete', function(req, res) {
     
 });
 
+//select  user
+app.get('/users/:id', function(req, res) {
+    var id = req.params.id;
+    var sql = 'select * from users';
+    if(id){
+        sql += ' where id ='+ id;
+    }
+  
+   db.any(sql)
+      .then(function(data){
+          console.log('DATA:'+ data);
+          res.render('pages/user_edit',{users : data[0]})
+  
+      })
+      .catch(function(error){
+          console.log('ERROR:'+ error);
+  
+      })
+  });
+
+
 
                  ///localhost
                  var port = process.env.PORT || 8080;
